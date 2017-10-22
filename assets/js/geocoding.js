d6d6fe0d9cfed6af1965b95bcb8e337ce836099c
Nguyễn Đình Sơn <001.IceTea@gmail.com>
2017-10-22 18:19:39 +0700

    Try to split insta tokken from url

--- assets/js/geocoding.js
+++ assets/js/geocoding.js
@@ -1,5 +1,16 @@
 function initMap() {
-        
+        var url = window.location.href;
+    
+        alert(url);
+        var token = new URL(url).hash.split('&').filter(function(el) { if(el.match('access_token') !== null) return true; })[0].split('=')[1];
+
+        alert(token);
+
+        if(access_token == "")
+            {
+                redirectToInstagAPI();
+            }
+    
         var geocoder = new google.maps.Geocoder();
 
         document.getElementById('SearchBtn').addEventListener('click', function() {
@@ -7,6 +18,11 @@ function initMap() {
         });
       }
 
+function redirectToInstagAPI()
+{
+    window.location.href = "https://www.instagram.com/oauth/authorize/?client_id=5ad0aa418c074e1d86fa698b83bd919e&redirect_uri=https://windyday.github.io/&response_type=token";
+    
+}
 function geocodeAddress(geocoder) {
     var address = document.getElementById('SearchField').value;
     geocoder.geocode({'address': address}, function(results, status) {
