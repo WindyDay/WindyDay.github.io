eeed925552e84d7dbec754422ac1c880a52e6a57
Nguyễn Đình Sơn <001.IceTea@gmail.com>
2017-10-22 20:56:09 +0700

    Fix previous function

--- assets/js/geocoding.js
+++ assets/js/geocoding.js
@@ -59,14 +59,10 @@ function generateNearbyList(lat, lng, tokken)
             
 		})
 
-		document.getElementById('listLocationNearBy ').innerHTML = html;
+		document.getElementById('listLocationNearBy').innerHTML = html;
 	});
     
-    <li>Item 1</li>
-    <li>Item 2</li>
-    <li>Item 3</li>
-    <li>Item 4</li>
-</ul>
+    html += '</ul>';
     
 }
 
