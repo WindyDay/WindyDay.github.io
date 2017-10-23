6831b386429d8a222679c5706c5a12201cdd6014
Nguyễn Đình Sơn <001.IceTea@gmail.com>
2017-10-23 15:36:23 +0700

    edit redirect url

--- ImageByLocation/assets/js/geocoding.js
+++ ImageByLocation/assets/js/geocoding.js
@@ -24,7 +24,7 @@ function initMap()
 
 function redirectToInstagAPI()
 {
-    window.location.href = "https://www.instagram.com/oauth/authorize/?client_id=5ad0aa418c074e1d86fa698b83bd919e&redirect_uri=https://windyday.github.io/&response_type=token&scope=public_content";
+    window.location.href = "https://www.instagram.com/oauth/authorize/?client_id=5ad0aa418c074e1d86fa698b83bd919e&redirect_uri=https://windyday.github.io/ImageByLocation&response_type=token&scope=public_content";
     
 }
 
