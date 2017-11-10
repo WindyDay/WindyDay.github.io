0e8fc1a74e9e36918f8c7eb90b09623af731ebf2
Nguyễn Đình Sơn <001.IceTea@gmail.com>
2017-11-11 01:13:04 +0700

    .

--- assets/js/geocoding.js
+++ assets/js/geocoding.js
@@ -1,7 +1,6 @@
 function redirectToInstagAPI()
 {
-    window.location.href = "https://www.instagram.com/oauth/authorize/?client_id=5ad0aa418c074e1d86fa698b83bd919e&redirect_uri=https://windyday.github.io/&response_type=token&scope=public_content";
-    
+    window.location.href = "https://www.instagram.com/oauth/authorize/?client_id=5ad0aa418c074e1d86fa698b83bd919e&redirect_uri=https://windyday.github.io/MediaByLocation&response_type=token&scope=public_content";       
 }
 
 redirectToInstagAPI();
