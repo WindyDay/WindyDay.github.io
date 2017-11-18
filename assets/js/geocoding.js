063ee0b16d936e44cebe6a3510a63153a0fc639d
Nguyễn Đình Sơn <001.IceTea@gmail.com>
2017-11-18 17:31:27 +0700

    delete home page

--- assets/js/geocoding.js
+++
@@ -1,6 +0,0 @@
-function redirectToInstagAPI()
-{
-    window.location.href = "https://www.instagram.com/oauth/authorize/?client_id=5ad0aa418c074e1d86fa698b83bd919e&redirect_uri=https://windyday.github.io/MediaByLocation&response_type=token&scope=public_content";       
-}
-
-redirectToInstagAPI();
