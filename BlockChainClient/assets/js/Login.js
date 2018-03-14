cb59835bf70b6d5795dc72e63d9bbb654a4b777a
Nguyễn Đình Sơn <001.IceTea@gmail.com>
2018-03-14 21:09:48 +0700

    publish Practice_PSD_to_HTML_Slicing

---
+++ BlockChainClient/assets/js/Login.js
@@ -0,0 +1,67 @@
+/* ------------------------------------ Click on login and Sign Up to  changue and view the effect
+---------------------------------------
+*/
+var inLogin = null;
+
+function cambiar_login() {
+  inLogin = true;
+  status();
+  document.querySelector('.cont_forms').className = "cont_forms cont_forms_active_login";
+  document.querySelector('.cont_form_login').style.display = "block";
+  document.querySelector('.cont_form_sign_up').style.opacity = "0";
+
+  setTimeout(function () {
+    document.querySelector('.cont_form_login').style.opacity = "1";
+  }, 400);
+
+  setTimeout(function () {
+    document.querySelector('.cont_form_sign_up').style.display = "none";
+  }, 200);
+}
+
+function cambiar_sign_up(at) {
+  if(inLogin === false){
+    // get data and do ajax
+  }
+
+
+  inLogin = false;
+  console.log("clicked sign up");
+  status();
+  document.querySelector('.cont_forms').className = "cont_forms cont_forms_active_sign_up";
+  document.querySelector('.cont_form_sign_up').style.display = "block";
+  document.querySelector('.cont_form_login').style.opacity = "0";
+
+  setTimeout(function () {
+    document.querySelector('.cont_form_sign_up').style.opacity = "1";
+  }, 100);
+
+  setTimeout(function () {
+    document.querySelector('.cont_form_login').style.display = "none";
+  }, 400);
+
+
+
+}
+
+
+
+function ocultar_login_sign_up() {
+
+  inLogin = null;
+  console.log("ocultar_login_sign_up()");
+  status();
+  document.querySelector('.cont_forms').className = "cont_forms";
+  document.querySelector('.cont_form_sign_up').style.opacity = "0";
+  document.querySelector('.cont_form_login').style.opacity = "0";
+
+  setTimeout(function () {
+    document.querySelector('.cont_form_sign_up').style.display = "none";
+    document.querySelector('.cont_form_login').style.display = "none";
+  }, 500);
+
+}
+
+function status() {
+  console.log("inLogin = " + inLogin);
+}
